e4c70b3b60792af7f679c1a4e9d6b0ae651551ed
Julen Pardo <julen.pardo@outlook.es>
2017-01-28 15:47:39 +0100

    i-#2: add test classes (not yet implemented)

---
+++ tests/git_wrapper_test.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class GitWrapperTest(unittest.TestCase):
+
+    def test_get_username_and_repo(self):
+        self.fail('Not yet implemented')
+
+    def test_get_remote_url(self):
+        self.fail('Not yet implemented')
+
+    def test_execute_command(self):
+        self.fail('Not yet implemented')
---
+++ tests/printer_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class PrinterTest(unittest.TestCase):
+
+    def test_print_issue_list(self):
+        self.fail('Not yet implemented')
+
+
+    def test_print_issue_list_with_desc(self):
+        self.fail('Not yet implemented')
