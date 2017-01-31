5bce39ab59a32cf161ebe75f309190f1a737ca36
Julen Pardo <julen.pardo@outlook.es>
2017-01-31 21:17:31 +0100

    i-#1: add dummy class for testing

---
+++ gitssue/remote/dummy.py
@@ -0,0 +1,49 @@
+""" Dummy module to simulate """
+
+from remote.remote_repo_interface import RemoteRepoInterface
+
+
+class Dummy(RemoteRepoInterface):
+
+    def get_issues_description(self, username, repository, issue_numbers):
+        return (
+            {
+                'number': '1',
+                'description': {
+                    'title': 'First dummy issue',
+                    'body': 'Body of first dummy issue.',
+                },
+            },
+            {
+                'number': '2',
+                'description': {
+                    'title': 'Second dummy issue',
+                    'body': 'Body of second dummy issue.',
+                },
+            },
+        )
+
+    def get_issue_list(self, username, repository, show_all=False, show_labels=False):
+        return (
+            {
+                'number': '1',
+                'title': 'First dummy issue',
+                'labels': (
+                    {
+                        'name': 'bug',
+                    },
+                    {
+                        'name': 'dummy',
+                    },
+                ),
+            },
+            {
+                'number': '2',
+                'title': 'Second dummy issue',
+                'labels': (
+                    {
+                        'name': 'dummy',
+                    },
+                ),
+            },
+        )
