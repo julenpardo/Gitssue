15115a76ebd5c82b96df91c30c9af2d96b9fae5d
Julen Pardo <julen.pardo@outlook.es>
2017-02-02 23:31:21 +0100

    i-#2: fix printer tests

--- tests/printer_test.py
+++ tests/printer_test.py
@@ -3,25 +3,44 @@ import sys, os
 sys.path.append(os.path.abspath('..'))
 import contextlib
 from io import StringIO
-from printer.printer import Printer
+from gitssue.printer.printer import Printer
+from gitssue.printer.color_printer_interface import ColorPrinterInterface
+
+
+class DummyColorPrinter(ColorPrinterInterface):
+    def print_colored_line(self, line, hex_color='ffffff'):
+        print(line)
+
+    def print_labels(self, labels):
+        print(' '.join(labels))
 
 
 class PrinterTest(unittest.TestCase):
 
     def setUp(self):
-        self.printer = Printer()
+        color_printer = DummyColorPrinter()
+        self.printer = Printer(color_printer)
 
     def test_print_issue_list(self):
-        issues_input = {
-            '1': 'first issue',
-            '2': 'second issue',
-            '3': 'third and last issue'
-        }
+        issues_input = (
+            {
+                'number': '1',
+                'title': 'first issue',
+            },
+            {
+                'number': '2',
+                'title': 'second issue',
+            },
+            {
+                'number': '3',
+                'title': 'third and last issue',
+            },
+        )
 
         expected = ''
 
-        for number, label in sorted(issues_input.items()):
-            expected += '#{0}: {1}\n'.format(number, label)
+        for issue in issues_input:
+            expected += '#{0}: {1}\n'.format(issue['number'], issue['title'])
 
         expected = expected[:-1]
 
@@ -63,8 +82,9 @@ class PrinterTest(unittest.TestCase):
         for issue in issues_input:
             expected += '#{0}: {1}\n'.format(issue['number'], issue['description']['title'])
             expected += '{0}\n'.format(issue['description']['body'])
-            expected += '-----------------------------------\n\n'
-        expected = expected[:-2]
+            expected += '\n\n'
+
+        expected = expected[:-3]
 
         temp_stdout = StringIO()
         with contextlib.redirect_stdout(temp_stdout):
