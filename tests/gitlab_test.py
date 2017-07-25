23650be0ed06a78e565d149689e4cc2617e810b9
Julen Pardo <julen.pardo@outlook.es>
2017-07-25 16:32:36 +0200

    ft #20: test "get_issue_list"

--- tests/gitlab_test.py
+++ tests/gitlab_test.py
@@ -22,6 +22,58 @@ class GitlabTest(unittest.TestCase):
     def mock_get_request_with_error(self, request, credentials={}):
         return False
 
+    def test_get_issue_list(self):
+        project_id = {'id': 1}
+        mocked_issue_list = [
+            {
+                'iid': '1',
+                'title': 'first issue',
+                'labels': [
+                    'bug',
+                    'feature'
+                ],
+                'body': 'body 1',
+            },
+        ]
+
+        def side_effect(*args, **kwargs):
+            labels = [
+                {
+                    'name': 'bug',
+                    'color': '#f0f0f0',
+                }, {
+                    'name': 'feature',
+                    'color': '#ffffff'
+                }
+            ]
+            if args[0] == 'https://gitlab.com/api/v4/issues':
+                return mocked_issue_list
+            elif args[0] == 'https://gitlab.com/api/v4/projects/username%2Frepo':
+                return project_id
+            elif args[0] == 'https://gitlab.com/api/v4/projects/1/labels':
+                return labels
+
+        requester_mock = mock.Mock()
+        requester_mock.get_request.side_effect = side_effect
+        gitlab = Gitlab(requester_mock, credentials=self.CREDENTIALS)
+
+        expected = [{
+            'number': '1',
+            'title': 'first issue',
+            'labels': [{
+                'name': 'bug',
+                'color': 'f0f0f0',
+            }, {
+                'name': 'feature',
+                'color': 'ffffff'
+            }],
+            'description': ''
+        }]
+
+        actual = gitlab.get_issue_list('username', 'repo')
+
+        self.assertEqual(expected, actual)
+
     def test_get_project_id(self):
         mocked_return = {
             'id': 12345,
