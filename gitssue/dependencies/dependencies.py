065d8280d5f15bc67535ce3b5e16c3b19ea42b79
Julen Pardo <julen.pardo@outlook.es>
2017-08-31 16:24:01 +0200

    eh #27: convert git wrapper to a class

--- gitssue/dependencies/dependencies.py
+++ gitssue/dependencies/dependencies.py
@@ -2,6 +2,7 @@
 from gitssue.request.requests import Requests
 
 from gitssue.git.shell_wrapper import ShellWrapper
+from gitssue.git.git_wrapper import GitWrapper
 from gitssue.printer.colorconsole_color_printer import ColorConsoleColorPrinter
 from gitssue.printer.printer import Printer
 from gitssue.remote.github import Github
@@ -19,6 +20,8 @@ class Dependencies:
     remote = Gitlab(requester, config_reader.get_config())
 
     shell = ShellWrapper()
+
+    git_wrapper = GitWrapper(shell)
 
     color_printer = ColorConsoleColorPrinter()
 
