db52707fa7eb1c4449e51148963932775f26bafa
Julen Pardo <julen.pardo@outlook.es>
2018-06-16 13:35:58 +0200

    en #37: move command definitions to gitssue.py

--- gitssue/__main__.py
+++ gitssue/__main__.py
@@ -2,49 +2,11 @@
 
 import sys
 import os
-import click
 
 sys.path.insert(0, os.getcwd())
 
-from gitssue.gitssue import Gitssue
+from gitssue import gitssue
 
-# gitssue = Gitssue()
-# gitssue.run()
-
-
-@click.group()
-def cli():
-    pass
-
-
-@click.command(help='List open issues.')
-@click.option('--all', '-a', is_flag=True, help='Show also closed issues.')
-def list(all):
-    print(all)
-
-
-@click.command(help='Get description of specified issue(s).')
-@click.argument('issues', nargs=-1)
-def desc(issues):
-    print(issues)
-
-
-@click.command(help='Get the comments of specified issue.')
-@click.argument('issue', nargs=1, type=click.INT)
-def thread(issue):
-    print(issue)
-
-
-@click.command(help='Shows the API rate information (remaining requests, reset'
-                    ' time, etc.).')
-def rate_info():
-    print('rate info')
-
-
-cli.add_command(list)
-cli.add_command(desc)
-cli.add_command(thread)
-cli.add_command(rate_info)
 
 if __name__ == '__main__':
-    cli()
+    gitssue.cli()
--- gitssue/gitssue.py
+++ gitssue/gitssue.py
@@ -3,6 +3,7 @@
 
 import sys
 import os
+import click
 
 sys.path.insert(0, os.getcwd())
 
@@ -11,12 +12,54 @@ from gitssue.controller.controller import Controller
 
 GITSSUE_VERSION = '1.3'
 
+controller = Controller(Dependencies())
+
+def print_version(context, param, value):
+    if not value or context.resilient_parsing:
+        return
+    print('Gitssue {0}'.format(GITSSUE_VERSION))
+    context.exit()
 
 
-class Gitssue():
-    """
-    Main class.
-    """
+@click.group()
+@click.option('--version', is_flag=True, callback=print_version,
+              expose_value=False, is_eager=True, help='Show version and exit.')
+def cli():
+    pass
 
-    def run(self):
-        print('run')
+
+@click.command(help='List open issues.')
+@click.option('--all', '-a', is_flag=True, help='Show also closed issues.')
+@click.option('--desc', '-d', is_flag=True,
+              help='Get description of the issues.')
+def list(all, desc):
+    controller.list(all, desc)
+
+
+@click.command(help='Get description of specified issue(s).')
+@click.argument('issues', nargs=-1)
+@click.pass_context
+def desc(context, issues):
+    if len(issues) == 0:
+        print('Usage: gitssue desc [OPTIONS] [issue [issue ...]]\n')
+        print('Error: Missing argument "issue".')
+        context.exit(2)
+    controller.desc(issues)
+
+
+@click.command(help='Get the comments of specified issue.')
+@click.argument('issue', nargs=1, type=click.INT)
+def comments(issue):
+    controller.thread(issue)
+
+
+@click.command(help='Shows the API rate information (remaining requests, reset'
+                    ' time, etc.).')
+def rate_info():
+    controller.rate_information()
+
+
+cli.add_command(list)
+cli.add_command(desc)
+cli.add_command(comments)
+cli.add_command(rate_info)
